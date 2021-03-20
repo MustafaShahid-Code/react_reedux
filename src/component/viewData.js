bbf474e4aa6c5f424f05ffe200e705c13e17ec0d
Mustafa Shahid <mustafashahid78692@gmail.com>
2021-03-20 11:32:58 +0300

    Login Form added

---
+++ src/component/viewData.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+class viewData extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <h1>Email:</h1>
+        <h1>Password:</h1>
+      </div>
+    )
+  }
+}
+export default viewData
