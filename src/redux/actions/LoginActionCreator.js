f085c3c51f466ad9ddc0e526b4536d923778f5e8
Mustafa Shahid <mustafashahid78692@gmail.com>
2021-03-21 11:00:02 +0300

    Redux Thunk test

--- src/redux/actions/LoginActionCreator.js
+++ src/redux/actions/LoginActionCreator.js
@@ -1,8 +1,17 @@
 import { LOGIN_REQUEST } from "./types"
 
 export const loginUser = (payload) => {
-  return {
-    type: LOGIN_REQUEST,
-    payload: payload,
+  return function (dispatch) {
+    testDelay()
+
+    dispatch({
+      type: LOGIN_REQUEST,
+      payload: payload,
+    })
   }
+}
+function testDelay() {
+  setTimeout(function () {
+    console.log("Hello from testDelay...")
+  }, 5000)
 }
